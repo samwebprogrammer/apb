d4a18c55fff26ea36db62bde9dc24eb7b26fe800
samwebprogrammer <syaifulalim88@gmail.com>
2023-11-05 10:17:06 +0700

    Menggunakan framework ci4

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+/**--ori--
+ * $routes->get('dashboard/index', 'Dashboard::index');*/
+
+$routes->get('dashboard', 'Dashboard::index');
+
+
